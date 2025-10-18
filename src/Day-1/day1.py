1311c76f9530d424c582ecc5e864543a1efc9369
Harvey-beep <hlviprofessionals@gmail.com>
2025-10-18 13:40:13 +0800

    feat: full progress for day 1

---
+++ src/Day-1/day1.py
@@ -0,0 +1,9 @@
+# Variables store data
+test_case_id = "TC-001"         # String
+steps = 5                       # Integer
+is_passed = True                # Boolean (true/false)
+
+# print() displays values in the terminal
+print("Test Case ID:", test_case_id)
+print("Number of Steps:", steps)
+print("Status (Passed):", is_passed)
---
+++ src/Day-1/test_report.py
@@ -0,0 +1,11 @@
+# Day 1 Mini Project
+
+test_name = "TC-001"
+browser = "Chrome"
+run_time = 2.5
+errors_found = 0
+
+print("Test Name:", test_name)
+print("Browser Used:", browser)
+print("Run Time in Minutes:", run_time, "minutes.")
+print(f"There are {errors_found} errors found.")
